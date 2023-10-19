23fabb6b1edfebaf1bca6d5498fcb809346cd3f1
ericjiang02 <ericjiang02@gmail.com>
2023-10-19 21:21:47 +1300

    configuring the cucumber runner class to point to feature file

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -3,11 +3,14 @@ package runners;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import io.cucumber.java.Before;
 import io.cucumber.java.After;
 
+@CucumberOptions(features = "src/test/resources/features", // Adjust the path as needed
+        glue = {"steps"}) // Package containing step definitions
 public class TestRunner extends AbstractTestNGCucumberTests {
 
 }
