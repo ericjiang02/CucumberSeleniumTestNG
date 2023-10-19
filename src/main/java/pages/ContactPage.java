a45570d32c06f574df7e4edfa48cea16aedb194f
ericjiang02 <ericjiang02@gmail.com>
2023-10-19 21:26:11 +1300

    Setting explicit wait for error message elements on ContactPage

--- src/main/java/pages/ContactPage.java
+++ src/main/java/pages/ContactPage.java
@@ -1,11 +1,21 @@
 package pages;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
 
 public class ContactPage extends BasePage {
+
+    private static WebDriverWait waitDriver;
+    public final static int TIMEOUT = 5;
     public ContactPage(WebDriver driver) {
         super(driver);
+        waitDriver = new WebDriverWait(driver, Duration.ofSeconds(TIMEOUT));
     }
 
     @FindBy(id = "forename")
@@ -43,18 +53,22 @@ public class ContactPage extends BasePage {
     }
 
     public boolean isForenameErrorMessageDisplayed() {
+        waitDriver.until(ExpectedConditions.visibilityOf(forenameIsRequiredErrorMessage));
         return forenameIsRequiredErrorMessage.isDisplayed();
     }
 
     public boolean isEmailErrorMessageDisplayed() {
+        waitDriver.until(ExpectedConditions.visibilityOf(emailIsRequiredErrorMessage));
         return emailIsRequiredErrorMessage.isDisplayed();
     }
 
     public boolean isMessageErrorMessageDisplayed() {
+        waitDriver.until(ExpectedConditions.visibilityOf(messageIsRequiredErrorMessage));
         return messageIsRequiredErrorMessage.isDisplayed();
     }
 
     public String getSuccessMessageText() {
+        waitDriver.until(ExpectedConditions.visibilityOf(successMessage));
         return successMessage.getText();
     }
 }
